daaac31e0f21b2d068d116ef5cd723148dca3941
xubingbing <xubingbing@chngdz.com>
2023-08-24 00:18:46 +0800

    进程

--- reptile/进程和线程/多个子进程.py
+++ reptile/进程和线程/多个子进程.py
@@ -1,14 +1,18 @@
 from multiprocessing import Process
 
 
+def get_data(url):
+  print(url)
 
 
 
 
 
-
-
-
+if __name__ == '__main__':
+   url = 'http://www.baidu.com?page='
+   for i in range(10):
+       new_url = url + str(i)
+       Process(target=get_data,args=(new_url,)).start()
 
 
 
